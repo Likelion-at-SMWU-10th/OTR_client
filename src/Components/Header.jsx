1a2f1a31411d3b42af535eaa95bfe798348bf275
Heesoo <hilarious_u@sookmyung.ac.kr>
2022-07-21 18:21:12 +0900

    로고 삽입

---
+++ src/Components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+
+class Header extends React.Component{
+  render(){
+    return (
+    <>
+      <div className="app-header">
+        ㅤㅤㅤㅤ
+      </div>
+      <div className="app-logo">
+        <img className="app-logo" alt="logo" src="img/Logo.png" />
+      </div>
+      </>
+    )
+  }
+}
+    
+export default Header;
