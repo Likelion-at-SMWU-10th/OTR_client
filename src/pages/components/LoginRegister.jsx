55dc0a56cef250d602762d159e876a05edd02557
Heesoo <hilarious_u@sookmyung.ac.kr>
2022-07-22 04:00:41 +0900

    component에 page 연결

--- src/pages/components/LoginRegister.jsx
+++ src/pages/components/LoginRegister.jsx
@@ -1,12 +1,15 @@
 import Badge from 'react-bootstrap/Badge';
 import './LoginRegister.css';
+import { Link } from 'react-router-dom';
 
 function PillExample() {
   return (
     <div className="Buttons">
+      <Link to = "/login">
       <Badge pill bg="light" text="dark" className="lightbutton">
       ㅤLog inㅤ
-      </Badge>{' '}
+      </Badge></Link>
+      {' '}
       <Badge pill bg="dark" className="darkbutton">
       ㅤRegisterㅤ
       </Badge>
