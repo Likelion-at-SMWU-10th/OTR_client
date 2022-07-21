b5427f60152ac0ce3e03e9e184aec54310ed49fe
Heesoo <hilarious_u@sookmyung.ac.kr>
2022-07-21 18:25:39 +0900

    js파일 수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import './App.css';
+import React from 'react';
+import './Header.css';
 
-function App() {
-  return <h3>Hello World</h3>;
+class Header extends React.Component{
+  render(){
+    return (
+    <>
+      <div className="app-header">
+        ㅤㅤㅤㅤ
+      </div>
+      <div className="app-logo">
+        <img className="app-logo" alt="logo" src="img/Logo.png" />
+      </div>
+      </>
+    )
+  }
 }
-export default App;
+    
+export default Header;
