4d8351d93a8544dd4e800b7e8c7d9668dacef7d7
Heesoo <hilarious_u@sookmyung.ac.kr>
2022-07-22 21:44:47 +0900

    About Otr 페이지 컨텐츠 css 추가

--- src/pages/AboutOtr.jsx
+++ src/pages/AboutOtr.jsx
@@ -5,17 +5,19 @@ import LoginRegister from './components/LoginRegister';
 import OtrDetail from './pageimg/OtrDetail.png';
 import OtrCard from './pageimg/OtrCard.png';
 import StartButton from './pageimg/StartButton.png';
+import './AboutOtr.css'
 
 const AboutOtr = () => {
     return (
         <>
         <Header/>
         <LoginRegister/>
-        <div><img src={OtrDetail} /></div>
-        <div><img src={OtrCard} /></div>
-        <div><img src={StartButton} /></div>
-        <h1> About OTR </h1>
-        <Link to ="/"><p>홈으로가기</p></Link>
+        <br/><br/><br/>
+        <div class="contents"><img src={OtrDetail} /></div>
+        <br/><br/><br/>
+        <div class="contents"><img className="otrcard" src={OtrCard} /></div>
+        <br/><br/><br/>
+        <div class="contents"><img src={StartButton} /></div>
         </>
     );
 };
