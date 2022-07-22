58ab0af2f654d8137cc780feee2e78f119cf5700
Heesoo <hilarious_u@sookmyung.ac.kr>
2022-07-22 21:34:29 +0900

    About Otr 페이지 컨텐츠 추가

--- src/pages/AboutOtr.jsx
+++ src/pages/AboutOtr.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from './components/Header';
 import LoginRegister from './components/LoginRegister';
+import OtrDetail from './pageimg/OtrDetail.png';
+import OtrCard from './pageimg/OtrCard.png';
+import StartButton from './pageimg/StartButton.png';
 
 const AboutOtr = () => {
     return (
         <>
         <Header/>
         <LoginRegister/>
+        <div><img src={OtrDetail} /></div>
+        <div><img src={OtrCard} /></div>
+        <div><img src={StartButton} /></div>
         <h1> About OTR </h1>
         <Link to ="/"><p>홈으로가기</p></Link>
         </>
