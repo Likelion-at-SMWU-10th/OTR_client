3faacc4c740ec1307813588961f53f72ceea408b
Heesoo <hilarious_u@sookmyung.ac.kr>
2022-07-22 21:16:05 +0900

    About Otr 페이지 헤더, 로그인버튼 추가

--- src/pages/AboutOtr.jsx
+++ src/pages/AboutOtr.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Header from './components/Header';
+import LoginRegister from './components/LoginRegister';
 
 const AboutOtr = () => {
     return (
         <>
+        <Header/>
+        <LoginRegister/>
         <h1> About OTR </h1>
         <Link to ="/"><p>홈으로가기</p></Link>
         </>
