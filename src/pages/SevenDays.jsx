24574c2cb72610346c88d8b39056bc61ad6fa74a
Heesoo <hilarious_u@sookmyung.ac.kr>
2022-07-23 11:11:37 +0900

    7일 문답페이지 notice 문구추가

--- src/pages/SevenDays.jsx
+++ src/pages/SevenDays.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from './components/Header';
-
+import SevenDayNotice from './pageimg/SevenDayNotice.png';
 const LoginPage = () => {
     return (
         <>
         <Header/>
-        <h1> 7일문답작성페이지 입니다! </h1>
-        <Link to ="/"><p>홈으로가기</p></Link>
+        <br/><br/><br/>
+        <div class="notice"><img alt="notice"src={SevenDayNotice} /></div>
         </>
     );
 };
