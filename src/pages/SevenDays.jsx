0a841044986d036276b0ce5e1463d3d6be7aeca6
jiyoon <giyun3632@sookmyung.ac.kr>
2022-07-25 14:22:41 +0900

    Footer 추가

--- src/pages/SevenDays.jsx
+++ src/pages/SevenDays.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 const LoginPage = () => {
     return (
@@ -8,6 +9,7 @@ const LoginPage = () => {
         <Header/>
         <h1> 7일문답작성페이지 입니다! </h1>
         <Link to ="/"><p>홈으로가기</p></Link>
+        <Footer />
         </>
     );
 };
