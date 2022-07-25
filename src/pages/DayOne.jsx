95bace6152858986012beadbc4c5095bc05c04b1
Heesoo <hilarious_u@sookmyung.ac.kr>
2022-07-25 14:33:31 +0900

    수정

--- src/pages/DayOne.jsx
+++ src/pages/DayOne.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from './components/Header';
 import SevenDayNotice from './pageimg/SevenDayNotice.png';
-import './SevenDays.css';
+import './DayOne.css';
+import SevenDaysCard from './components/SevenDaysCard';
 
-
-const SevenDays = () => {
+const LoginPage = () => {
     return (
         <>
         <Header/>
         <br/><br/><br/>
         <div class="notice"><img alt="notice"src={SevenDayNotice} /></div>
         <br/><br/><br/>
+        <SevenDaysCard/>
+        <br/><br/><br/>
         </>
     );
 };
 
-export default SevenDays;
+export default LoginPage;
