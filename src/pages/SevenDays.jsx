2277102f8e75742e312083bb19b112e3305b3f19
Heesoo <hilarious_u@sookmyung.ac.kr>
2022-07-25 15:04:03 +0900

    About Otr 페이지 비율조정

--- src/pages/SevenDays.jsx
+++ src/pages/SevenDays.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from './components/Header';
-
-
 import Footer from './components/Footer';
 
 const SevenDays = () => {
     return (
         <>
         <Header/>
-
-        <h1> 7일문답작성페이지 입니다! </h1>
-        <Link to ="/"><p>홈으로가기</p></Link>
-        <Footer />
+        <Footer/>
         </>
     );
 };
