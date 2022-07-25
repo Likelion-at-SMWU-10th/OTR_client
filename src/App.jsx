becf76d7d5031c3c0ebdf114a61052e5d83fcc50
Heesoo <hilarious_u@sookmyung.ac.kr>
2022-07-25 23:02:20 +0900

    2일차 페이지 제작 완료

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AboutOtr from './pages/AboutOtr';
 import SevenDays from './pages/SevenDays';
 import LoginPage from './pages/LoginPage';
 import DayOne from './pages/DayOne';
+import DayTwo from './pages/DayTwo';
 
 class App extends Component {
   render() {
@@ -18,6 +19,7 @@ class App extends Component {
         <Route path="/sevendays" element={<SevenDays/>}></Route>
         <Route path="/login" element={<LoginPage/>}></Route>
         <Route path="/dayone" element={<DayOne/>}></Route>
+        <Route path="/daytwo" element={<DayTwo/>}></Route>
       </Routes>
       </>
     );
---
+++ src/pages/DayTwo.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Header from './components/Header';
+import SevenDaysNotice from './pageimg/SevenDaysNotice.png';
+import './DayOne.css';
+import DayTwoCard from './components/DayTwoCard';
+
+const DayTwo = () => {
+    return (
+        <>
+        <Header/>
+        <br/><br/><br/>
+        <div class="notice"><img alt="notice"src={SevenDaysNotice} /></div>
+        <br/><br/><br/>
+        <DayTwoCard/>
+        <br/><br/><br/>
+        </>
+    );
+};
+
+export default DayTwo;
