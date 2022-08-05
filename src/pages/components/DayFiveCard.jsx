ca9ed0474184dd49624024fbae8980369c5c0f86
Heesoo <hilarious_u@sookmyung.ac.kr>
2022-08-05 15:51:13 +0900

    day 5 modal 추가

--- src/pages/components/DayFiveCard.jsx
+++ src/pages/components/DayFiveCard.jsx
@@ -1,7 +1,7 @@
 import Card from 'react-bootstrap/Card';
 import './DayOneCard.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import AnswerBox1 from './AnswerBox1';
+import AnswerBox5 from './AnswerBox5';
 
 function DayFiveCard() {
   return (
@@ -12,11 +12,11 @@ function DayFiveCard() {
         <br/><br/>
         <Card.Title>
             <div className= "title" >
-            Q5. 다섯번째 질문
+            Q5. 나의 삶은 어떤 의미를 가지고 있나요? 나의 가치관은 무엇인가요?
             </div>
             <br/><br/>
             <div className= "answer">
-            <AnswerBox1/>
+            <AnswerBox5/>
             </div> 
         </Card.Title>
       </Card.Body>
